35c7e0e483efec1765f8fa983436cbc703d4fc0b
Hamit Sehjal <73131093+hamitsehjal@users.noreply.github.com>
2023-02-15 15:24:11 -0500

    Create Utilities.h

---
+++ HeaderFiles/Utilities.h
@@ -0,0 +1,23 @@
+
+
+#pragma once
+#ifndef SDDS_UTILITIES_H
+#define SDDS_UTILITIES_H
+
+namespace sdds
+{
+	class Utilities {
+		size_t m_widthField{ 1 };
+		static char m_delimiter;
+	public:
+		void setFieldWidth(size_t newWidth);
+		size_t getFieldWidth() const;
+		std::string extractToken(const std::string& str, size_t& next_pos, bool& more);
+		static void setDelimiter(char newDelimiter);
+		static char getDelimiter();
+		~Utilities();
+	};
+}
+
+#endif // !SDDS_UTILITIES_H
+
